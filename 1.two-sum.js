2051c75a84f80a6ac9730c54b5dfb8ff1b002a57
satyam <satyamsingh748846@gmail.com>
2025-01-22 03:08:12 +0530

    feat: added leetcode two sum second sol

--- 1.two-sum.js
+++ 1.two-sum.js
@@ -12,12 +12,31 @@
  */
 var twoSum = function (nums, target) {
   const arrLength = nums.length;
+
+  // solution:01
   for (let i = 0; i < arrLength - 1; i++) {
     let newTraget = target - nums[i];
     for (let j = i + 1; j < arrLength; j++) {
       if (nums[j] === newTraget) {
         return [i, j];
       }
+    }
+  }
+
+  
+
+  // solution:02
+
+  const map = new Map();
+  for (let i = 0; i < arrLength; i++) {
+    map.set(nums[i], i);
+  }
+
+  for (let i = 0; i < arrLength; i++) {
+    const newTraget = target - nums[i];
+    const isNewTargetExistInMap = map.has(newTraget);
+    if (isNewTargetExistInMap && map.get(newTraget) !== i) {
+      return [i, map.get(newTraget)];
     }
   }
 
